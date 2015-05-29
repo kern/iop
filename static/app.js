bc3ed8b99a321a25829354fa72f49f08ac004985
Alex Kern <alex@kern.io>
2015-05-29 22:03:04 +0200

    Add basic layout for the app.

--- static/app.js
+++ static/app.js
@@ -1,1 +1,41 @@
-// alert('foo')
+var App = React.createClass({
+  render: function () {
+    return <main>
+      <MapPanel />
+      <InfoPanel />
+    </main>
+  }
+})
+
+var MapPanel = React.createClass({
+
+  render: function () {
+    return <div className="map-panel">
+      This is where the map goes.
+    </div>
+  }
+
+})
+
+var InfoPanel = React.createClass({
+
+  getInitialState: function () {
+    return {
+      messages: ['foo', 'bar']
+    }
+  },
+
+  render: function () {
+    return <div className="info-panel">
+      <h2>Information Feed</h2>
+      <ul>
+        {this.state.messages.map(function (m) {
+          return <li key={m}>{m}</li>
+        })}
+      </ul>
+    </div>
+  }
+
+})
+
+React.render(<App />, document.getElementById('app'));
