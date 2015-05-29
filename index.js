588b2ba0a9335a04fe9c463cc246896fb92c8ce8
Alex Kern <alex@kern.io>
2015-05-29 20:25:34 +0200

    Add SCSS.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express')
+var path = require('path')
+var sassMiddleware = require('node-sass-middleware')
 
 var app = express()
 
-app.use(express.static(__dirname + '/static'))
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'css'),
+  dest: path.join(__dirname, 'static'),
+  outputStyle: 'compressed'
+}))
+
+app.use(express.static(path.join(__dirname, 'static')))
 
 var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address
